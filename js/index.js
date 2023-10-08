d98e63cd5d08835cd6e8460aa64bd63e19e233dd
shrey0404 <shreybhatnagar6410@gmail.com>
2023-10-08 15:42:08 +0530

    implementation coding

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("Hello ji!");
+mport * as CANNON from 'https://cdn.skypack.dev/cannon-es';
+
+import * as THREE from 'three';
+import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
+
+const canvasEl = document.querySelector('#canvas');
+const scoreResult = document.querySelector('#score-result');
+const rollBtn = document.querySelector('#roll-btn');
+
+let renderer, scene, camera, diceMesh, physicsWorld;
