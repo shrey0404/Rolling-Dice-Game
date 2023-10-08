764db0e6e675db403a347f5eaf21343ac5308995
shrey0404 <shreybhatnagar6410@gmail.com>
2023-10-08 15:54:18 +0530

    implementing dice

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-mport * as CANNON from 'https://cdn.skypack.dev/cannon-es';
+import * as CANNON from 'https://cdn.skypack.dev/cannon-es';
 
 import * as THREE from 'three';
 import * as BufferGeometryUtils from 'three/addons/utils/BufferGeometryUtils.js';
@@ -127,4 +127,167 @@ function createDice() {
     physicsWorld.addBody(body);
 
     return {mesh, body};
+}
+
+function createBoxGeometry() {
+
+    let boxGeometry = new THREE.BoxGeometry(1, 1, 1, params.segments, params.segments, params.segments);
+
+    const positionAttr = boxGeometry.attributes.position;
+    const subCubeHalfSize = .5 - params.edgeRadius;
+
+
+    for (let i = 0; i < positionAttr.count; i++) {
+
+        let position = new THREE.Vector3().fromBufferAttribute(positionAttr, i);
+
+        const subCube = new THREE.Vector3(Math.sign(position.x), Math.sign(position.y), Math.sign(position.z)).multiplyScalar(subCubeHalfSize);
+        const addition = new THREE.Vector3().subVectors(position, subCube);
+
+        if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position = subCube.add(addition);
+        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.y) > subCubeHalfSize) {
+            addition.z = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.x = subCube.x + addition.x;
+            position.y = subCube.y + addition.y;
+        } else if (Math.abs(position.x) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            addition.y = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.x = subCube.x + addition.x;
+            position.z = subCube.z + addition.z;
+        } else if (Math.abs(position.y) > subCubeHalfSize && Math.abs(position.z) > subCubeHalfSize) {
+            addition.x = 0;
+            addition.normalize().multiplyScalar(params.edgeRadius);
+            position.y = subCube.y + addition.y;
+            position.z = subCube.z + addition.z;
+        }
+
+        const notchWave = (v) => {
+            v = (1 / params.notchRadius) * v;
+            v = Math.PI * Math.max(-1, Math.min(1, v));
+            return params.notchDepth * (Math.cos(v) + 1.);
+        }
+        const notch = (pos) => notchWave(pos[0]) * notchWave(pos[1]);
+
+        const offset = .23;
+
+        if (position.y === .5) {
+            position.y -= notch([position.x, position.z]);
+        } else if (position.x === .5) {
+            position.x -= notch([position.y + offset, position.z + offset]);
+            position.x -= notch([position.y - offset, position.z - offset]);
+        } else if (position.z === .5) {
+            position.z -= notch([position.x - offset, position.y + offset]);
+            position.z -= notch([position.x, position.y]);
+            position.z -= notch([position.x + offset, position.y - offset]);
+        } else if (position.z === -.5) {
+            position.z += notch([position.x + offset, position.y + offset]);
+            position.z += notch([position.x + offset, position.y - offset]);
+            position.z += notch([position.x - offset, position.y + offset]);
+            position.z += notch([position.x - offset, position.y - offset]);
+        } else if (position.x === -.5) {
+            position.x += notch([position.y + offset, position.z + offset]);
+            position.x += notch([position.y + offset, position.z - offset]);
+            position.x += notch([position.y, position.z]);
+            position.x += notch([position.y - offset, position.z + offset]);
+            position.x += notch([position.y - offset, position.z - offset]);
+        } else if (position.y === -.5) {
+            position.y += notch([position.x + offset, position.z + offset]);
+            position.y += notch([position.x + offset, position.z]);
+            position.y += notch([position.x + offset, position.z - offset]);
+            position.y += notch([position.x - offset, position.z + offset]);
+            position.y += notch([position.x - offset, position.z]);
+            position.y += notch([position.x - offset, position.z - offset]);
+        }
+
+        positionAttr.setXYZ(i, position.x, position.y, position.z);
+    }
+
+
+    boxGeometry.deleteAttribute('normal');
+    boxGeometry.deleteAttribute('uv');
+    boxGeometry = BufferGeometryUtils.mergeVertices(boxGeometry);
+
+    boxGeometry.computeVertexNormals();
+
+    return boxGeometry;
+}
+function createInnerGeometry() {
+    const baseGeometry = new THREE.PlaneGeometry(1 - 2 * params.edgeRadius, 1 - 2 * params.edgeRadius);
+    const offset = .48;
+    return BufferGeometryUtils.mergeBufferGeometries([
+        baseGeometry.clone().translate(0, 0, offset),
+        baseGeometry.clone().translate(0, 0, -offset),
+        baseGeometry.clone().rotateX(.5 * Math.PI).translate(0, -offset, 0),
+        baseGeometry.clone().rotateX(.5 * Math.PI).translate(0, offset, 0),
+        baseGeometry.clone().rotateY(.5 * Math.PI).translate(-offset, 0, 0),
+        baseGeometry.clone().rotateY(.5 * Math.PI).translate(offset, 0, 0),
+    ], false);
+}
+
+function addDiceEvents(dice) {
+    dice.body.addEventListener('sleep', (e) => {
+
+        dice.body.allowSleep = false;
+
+        const euler = new CANNON.Vec3();
+        e.target.quaternion.toEuler(euler);
+
+        const eps = .1;
+        let isZero = (angle) => Math.abs(angle) < eps;
+        let isHalfPi = (angle) => Math.abs(angle - .5 * Math.PI) < eps;
+        let isMinusHalfPi = (angle) => Math.abs(.5 * Math.PI + angle) < eps;
+        let isPiOrMinusPi = (angle) => (Math.abs(Math.PI - angle) < eps || Math.abs(Math.PI + angle) < eps);
+
+
+        if (isZero(euler.z)) {
+            if (isZero(euler.x)) {
+                showRollResults(1);
+            } else if (isHalfPi(euler.x)) {
+                showRollResults(4);
+            } else if (isMinusHalfPi(euler.x)) {
+                showRollResults(3);
+            } else if (isPiOrMinusPi(euler.x)) {
+                showRollResults(6);
+            } else {
+                // landed on edge => wait to fall on side and fire the event again
+                dice.body.allowSleep = true;
+            }
+        } else if (isHalfPi(euler.z)) {
+            showRollResults(2);
+        } else if (isMinusHalfPi(euler.z)) {
+            showRollResults(5);
+        } else {
+            // landed on edge => wait to fall on side and fire the event again
+            dice.body.allowSleep = true;
+        }
+    });
+}
+
+function showRollResults(score) {
+    if (scoreResult.innerHTML === '') {
+        scoreResult.innerHTML += score;
+    } else {
+        scoreResult.innerHTML += ('+' + score);
+    }
+}
+
+function render() {
+    physicsWorld.fixedStep();
+
+    for (const dice of diceArray) {
+        dice.mesh.position.copy(dice.body.position)
+        dice.mesh.quaternion.copy(dice.body.quaternion)
+    }
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+
+function updateSceneSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
