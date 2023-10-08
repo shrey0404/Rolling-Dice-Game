28f6a31ca9dcead786f64be27bdadd52defa4480
shrey0404 <shreybhatnagar6410@gmail.com>
2023-10-08 15:44:52 +0530

    changes in js and css

--- js/index.js
+++ js/index.js
@@ -8,3 +8,60 @@ const scoreResult = document.querySelector('#score-result');
 const rollBtn = document.querySelector('#roll-btn');
 
 let renderer, scene, camera, diceMesh, physicsWorld;
+
+const params = {
+    numberOfDice: 2,
+    segments: 40,
+    edgeRadius: .07,
+    notchRadius: .12,
+    notchDepth: .1,
+};
+
+const diceArray = [];
+
+initPhysics();
+initScene();
+
+window.addEventListener('resize', updateSceneSize);
+window.addEventListener('dblclick', throwDice);
+rollBtn.addEventListener('click', throwDice);
+
+function initScene() {
+
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true,
+        canvas: canvasEl
+    });
+    renderer.shadowMap.enabled = true
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 300)
+    camera.position.set(0, .5, 4).multiplyScalar(7);
+
+    updateSceneSize();
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, .5);
+    scene.add(ambientLight);
+    const topLight = new THREE.PointLight(0xffffff, .5);
+    topLight.position.set(10, 15, 0);
+    topLight.castShadow = true;
+    topLight.shadow.mapSize.width = 2048;
+    topLight.shadow.mapSize.height = 2048;
+    topLight.shadow.camera.near = 5;
+    topLight.shadow.camera.far = 400;
+    scene.add(topLight);
+
+    createFloor();
+    diceMesh = createDiceMesh();
+    for (let i = 0; i < params.numberOfDice; i++) {
+        diceArray.push(createDice());
+        addDiceEvents(diceArray[i]);
+    }
+
+    throwDice();
+
+    render();
+}
